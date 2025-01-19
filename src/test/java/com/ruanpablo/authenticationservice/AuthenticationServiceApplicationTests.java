221a119755aeb082a313cbdb80f6031c8fb6dc0f
rpablo <222ruanpablo@gmail.com>
2025-01-18 22:31:33 -0300

    Creating Repo

---
+++ src/test/java/com/ruanpablo/authenticationservice/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruanpablo.authenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
